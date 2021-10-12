69aaa981a1ae88a6d9e13752250fd60354db6cdc
YassinBendella <yassin.bendella@student.ehb.be>
2021-10-12 11:43:06 +0100

    re:commit - probleem met git

---
+++ les2/team.js
@@ -0,0 +1,30 @@
+"use strict"
+
+
+
+class Team {
+
+constructor(teamname, trainer, roster)
+
+this.teamname = 'Example teamname';
+
+this.trainer = 'Example trainername';
+
+this.roster = [];
+
+
+
+
+describe() {
+
+return `${this.teamname} with trainer ${this.trainername}
+
+has these pokemons: ${[...this.pokemonTeam]} `;
+
+}
+
+}
+
+
+
+export default Team;
